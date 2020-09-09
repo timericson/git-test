ea6285f44f0bb56851fa9d21b7ed15e0559e581c
Tim Ericson <smallitsolutions1@gmail.com>
2020-09-09 20:20:31 +0930

    initial commit

---
+++ Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test
+//
+//  Created by Tim Ericson on 9/9/20.
+//  Copyright © 2020 Tim Ericson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
